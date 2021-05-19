53e5eb311201a8f0aca279842a69db4068b15d1e
Ryan <ryanemic@gmail.com>
2021-05-19 16:39:53 +0800

    better masking

--- src/lib/ionic-currency-mask.directive.ts
+++ src/lib/ionic-currency-mask.directive.ts
@@ -43,16 +43,20 @@ export class CurrencyMaskDirective {
   changeValue(event: any) {
 	let value = event.target.value;
 	if (value == '') {
-		return;
+		event.target.value = value = '0' + this.decimal + '00';
+		this.model.update.emit(value);
+		return true;
 	}
 	value = value + '';
-	value = parseInt(value.replace(/[\D]+/g, ''));
+	value = value.replace(/[\D]+/g, '');
 	value = value + '';
 	value = value.replace(/([0-9]{2})$/g, this.decimal + '$1');
 	var parts = value.toString().split(this.decimal);
+	if(parts[0] == '')
+	  parts[0] = '0';
+	parts[0] = parseInt(parts[0]).toString();
 	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, this.thousand);
 	value = parts.join(this.decimal);
-	console.log(value);
 	event.target.value = value;
 	this.model.update.emit(value);
 	return true;
